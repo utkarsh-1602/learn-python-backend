52feb3de4e1dffab280e0bf2ac04d8ed84fbaa5d
utkarsh-1602 <usking619@gmail.com>
2024-07-09 00:40:47 +0530

    .

--- python-for-backend/django/project2/my_tennis_club/members/views.py
+++
@@ -1,8 +0,0 @@
-from django.shortcuts import render
-from django.http import HttpResponse
-
-def myview01(request):
-    return HttpResponse("Hello world!")
-
-#But how can we execute the view? Well, we must call the view via a URL.
-
